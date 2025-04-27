ea1ff8ddc1a58087e1e1e8e0797bcde64238cca9
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2025-04-27 11:47:04 +0800

    Add writeIMEI and rearrange script

--- done.sh
+++ done.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-logfilename="outputOptimizationEnable_"
+logfilename="outputOptimizationDisable_"
 timestamp="$(date +%s)"
 logfilename="$logfilename$timestamp"
 exec > /mnt/media_rw/udisk1/$logfilename.txt 2>&1
@@ -19,37 +19,36 @@ am start -a android.settings.WPC_SETTINGS 2>/dev/null
 
 pm disable com.neusoft.ssp.ces.c4.car.assistant # QDLink
 
-pm disable com.neusoft.na.navigation # Navigation (Navigation app)
-pm disable ecarx.naviservice # LBSNavi (Navigation Service)
-pm disable com.ecarx.naviwidget # Navi (Navigation widget)
+# pm disable com.neusoft.shockwave.tboxclient # TBoxClient (Disables 4G, only disable if 4G subscription expired.)
 
-pm disable ecarx.weather # weather
-pm disable ecarx.upgrade # XCUpgradeService (Cannot connect to server)
-pm disable ecarx.membercenter # Member Centre (Cannot connect to server)
-pm disable com.ecarx.thememanager # Themes (Cannot connect to server)
-
-pm disable com.qihoo360.vehiclesafe # 360 Car Defender Basic Service (Qihoo 360)
-pm disable com.chima.vulcan # com.android.chimaster (Qihoo 360 backdoor)
-
-pm disable ecarx.autosecure # Clean Up (Memory Clean Up)
-pm disable com.neusoft.onlinemusic # JOOX Music
-
-pm disable com.android.onetimeinitializer # One Time Init
-pm disable com.android.providers.applications # Search Applications Provider
-pm disable com.android.webview # Android System WebView (Outdated, use Opera 66.6.3445.73002 or Kiwi 96.0.4664.46 or Edge 95.0.1020.55 for web browsing)
-pm disable com.mediatek.mtklogger # MTKLogger
-pm disable com.mediatek.fwk.plugin # FwkPlugin (Mediatek Plugin System)
-
-pm disable com.gosuncn.toolsInstall # ToolsInstall
-pm disable org.xwalk.core # XWalkCoreLibrary (Crosswalk Project Runtime)
-
-pm disable com.neusoft.dvrlogin # DVR (Geely Driving Video Recorder app)
-pm disable com.neusoft.cardvr # Driving record (Geely Driving Video Recorder)
+pm disable com.android.webview # Android System WebView (Outdated)
+# Use Opera 66.6.3445.73002 (Disable 'Copy and search') or Kiwi 96.0.4664.46 or Edge 95.0.1020.55 for web browsing
 
 pm disable ecarx.video # Video
 pm disable ecarx.photo # Photo
 
-# pm disable com.neusoft.shockwave.tboxclient # TBoxClient (Disables 4G, only disable if 4G subscription expired.)
+pm disable com.neusoft.na.navigation # Navigation (Navigation app, cannot connect to server)
+pm disable ecarx.naviservice # LBSNavi (Navigation Service)
+pm disable com.ecarx.naviwidget # Navi (Navigation widget)
+
+pm disable ecarx.weather # weather (Cannot connect to server)
+pm disable ecarx.upgrade # XCUpgradeService (Cannot connect to server)
+pm disable ecarx.membercenter # Member Centre (Cannot connect to server)
+pm disable com.ecarx.thememanager # Themes (Cannot connect to server)
+pm disable com.neusoft.dvrlogin # DVR (Geely Driving Video Recorder app)
+pm disable com.neusoft.cardvr # Driving record (Geely Driving Video Recorder)
+pm disable ecarx.autosecure # Clean Up (Memory Clean Up)
+pm disable com.neusoft.onlinemusic # JOOX Music
+
+pm disable com.qihoo360.vehiclesafe # 360 Car Defender Basic Service (Qihoo 360)
+pm disable com.chima.vulcan # com.android.chimaster (Qihoo 360 backdoor)
+pm disable com.android.onetimeinitializer # One Time Init
+pm disable com.android.providers.applications # Search Applications Provider
+pm disable com.mediatek.mtklogger # MTKLogger
+pm disable com.mediatek.fwk.plugin # FwkPlugin (Mediatek Plugin System)
+pm disable org.xwalk.core # XWalkCoreLibrary (Crosswalk Project Runtime)
+pm disable com.gosuncn.toolsInstall # ToolsInstall
+pm disable com.android.writeimei # writeIMEI
 
 
 echo "====== renaming the script folder ========"
